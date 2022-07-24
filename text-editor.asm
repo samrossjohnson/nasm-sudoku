0159184fafc704d33517c74f9b61f8adc59e263b
samrossjohnson <sam.ross.johnson@gmail.com>
2022-07-24 16:38:37 +0100

    read loop for stdin with 'Q' used to break and quit

--- text-editor.asm
+++ text-editor.asm
@@ -8,6 +8,19 @@
 
             section   .text
 
-_start:     mov       rdi, 0                  ; store exit code
+_start:     mov       rax, 0                  ; system call for read
+            mov       rdi, 0                  ; file handle 0 is stdin
+            mov       rsi, buffer             ; address of buffer
+            mov       rdx, 1                  ; number of bytes to read
+            syscall                           ; invoke operating system to do the read
+            movzx     rdi, byte [buffer]      ; the byte that was read.
+            cmp       rdi, 81                 ; decimal ascii code for 'Q'
+            je        exit                    ; exit
+            jmp       _start                  ; continue loop
+
+exit:       mov       rdi, 0                  ; store read exit code
             mov       rax, 60                 ; system call for exit
             syscall                           ; invoke operating system to exit
+
+            section   .bss
+buffer:     resb      1
