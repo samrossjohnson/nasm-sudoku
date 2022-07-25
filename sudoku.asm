e4902cf5504cdc9dce585c6168a0f6e0f282698c
samrossjohnson <sam.ross.johnson@gmail.com>
2022-07-25 17:40:46 +0100

    setup defines for existing system call numbers, file descriptors, and keycodes

--- sudoku.asm
+++ sudoku.asm
@@ -3,14 +3,18 @@
 ;
 ;     nasm -felf64 sudoku.asm && ld sudoku.o && ./a.out
 ; ----------------------------------------------------------------------------------------
+%define     keycode_Q 81
+
+%define     sys_read  0
+%define     sys_exit  60
+
+%define     fd_stdin  0
 
             global    _start
-
+            
             section   .text
-keycode_Q:  equ       81
-
-_start:     mov       rax, 0                  ; system call for read
-            mov       rdi, 0                  ; file handle 0 is stdin
+_start:     mov       rax, sys_read           ; system call for read
+            mov       rdi, fd_stdin           ; file handle 0 is stdin
             mov       rsi, buffer             ; address of buffer
             mov       rdx, 1                  ; number of bytes to read
             syscall                           ; invoke operating system to do the read
@@ -20,7 +24,7 @@ _start:     mov       rax, 0                  ; system call for read
             jmp       _start                  ; continue loop
 
 exit:       mov       rdi, 0                  ; store read exit code
-            mov       rax, 60                 ; system call for exit
+            mov       rax, sys_exit           ; system call for exit
             syscall                           ; invoke operating system to exit
 
             section   .bss
