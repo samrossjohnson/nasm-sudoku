1e39ef3194ada93c823fc610fb3fbb682089e408
samrossjohnson <sam.ross.johnson@gmail.com>
2022-07-29 10:37:00 +0100

    disable icanon and echo for terminal for the program lifetime

--- sudoku.asm
+++ sudoku.asm
@@ -6,28 +6,71 @@
 %define     keycode_Q 81
 
 %define     sys_read  0
+%define     sys_ioctl 16
 %define     sys_exit  60
 
 %define     fd_stdin  0
+
+%define     tcgets    0x5401
+%define     tcsets    0x5402
+%define     icanon    1<<1
+%define     echo      1<<3
 
             global    _start
 
             section   .text
 _start:
+            mov       rax, sys_ioctl          ; system call for ioctl
+            mov       rdi, fd_stdin           ; file descriptor
+            mov       rsi, tcgets             ; request to get terminal attributes
+            mov       rdx, termios            ; buffer to write into
+            syscall                           ; invoke the OS to read termios
+
+            and       byte [c_lflag], ~echo   ; disable echo
+            and       byte [c_lflag], ~icanon ; disable canonical mode
+
+            mov       rax, sys_ioctl          ; system call for ioctl
+            mov       rdi, fd_stdin           ; file descriptor
+            mov       rsi, tcsets             ; request number
+            mov       rdx, termios            ; termios stucture to write
+            syscall                           ; invoke the OS to write termios
+
             mov       rax, sys_read           ; system call for read
             mov       rdi, fd_stdin           ; file handle 0 is stdin
             mov       rsi, buffer             ; address of buffer
             mov       rdx, 1                  ; number of bytes to read
-            syscall                           ; invoke operating system to do the read
+            syscall                           ; invoke OS to do the read
             movzx     rdi, byte [buffer]      ; the byte that was read
             cmp       rdi, keycode_Q          ; compare entered key with quit symbol
             je        exit                    ; exit
             jmp       _start                  ; continue loop
 
 exit:
+            mov       rax, sys_ioctl          ; system call for ioctl
+            mov       rdi, fd_stdin           ; file descriptor
+            mov       rsi, tcgets             ; request to get terminal attributes
+            mov       rdx, termios            ; buffer to write into
+            syscall                           ; invoke the OS to read termios
+
+            or        byte [c_lflag], echo    ; disable echo
+            or        byte [c_lflag], icanon  ; disable canonical mode
+
+            mov       rax, sys_ioctl          ; system call for ioctl
+            mov       rdi, fd_stdin           ; file descriptor
+            mov       rsi, tcsets             ; request number
+            mov       rdx, termios            ; termios stucture to write
+            syscall                           ; invoke the OS to write termios
+
             mov       rdi, 0                  ; store read exit code
             mov       rax, sys_exit           ; system call for exit
             syscall                           ; invoke operating system to exit
 
             section   .bss
 buffer:     resb      1
+termios:
+c_iflag     resd      1                       ; input mode flags
+c_oflag     resd      1                       ; output mode flags
+c_cflag     resd      1                       ; control mode flags
+c_lflag     resd      1                       ; local mode flags
+c_line      resb      1                       ; line discipline
+c_cc        resb      19                      ; control characters
