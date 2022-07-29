a978c226f5628bd5c9c24350f2e3521c91115ec1
samrossjohnson <sam.ross.johnson@gmail.com>
2022-07-29 10:47:33 +0100

    moved terminal setup and restoration to functions

--- sudoku.asm
+++ sudoku.asm
@@ -20,6 +20,24 @@
 
             section   .text
 _start:
+            call      init_term
+            mov       rax, sys_read           ; system call for read
+            mov       rdi, fd_stdin           ; file handle 0 is stdin
+            mov       rsi, buffer             ; address of buffer
+            mov       rdx, 1                  ; number of bytes to read
+            syscall                           ; invoke OS to do the read
+            movzx     rdi, byte [buffer]      ; the byte that was read
+            cmp       rdi, keycode_Q          ; compare entered key with quit symbol
+            je        exit                    ; exit
+            jmp       _start                  ; continue loop
+
+exit:
+            call      reset_term
+            mov       rdi, 0                  ; store read exit code
+            mov       rax, sys_exit           ; system call for exit
+            syscall                           ; invoke operating system to exit
+
+init_term:
             mov       rax, sys_ioctl          ; system call for ioctl
             mov       rdi, fd_stdin           ; file descriptor
             mov       rsi, tcgets             ; request to get terminal attributes
@@ -34,18 +52,9 @@ _start:
             mov       rsi, tcsets             ; request number
             mov       rdx, termios            ; termios stucture to write
             syscall                           ; invoke the OS to write termios
+            ret
 
-            mov       rax, sys_read           ; system call for read
-            mov       rdi, fd_stdin           ; file handle 0 is stdin
-            mov       rsi, buffer             ; address of buffer
-            mov       rdx, 1                  ; number of bytes to read
-            syscall                           ; invoke OS to do the read
-            movzx     rdi, byte [buffer]      ; the byte that was read
-            cmp       rdi, keycode_Q          ; compare entered key with quit symbol
-            je        exit                    ; exit
-            jmp       _start                  ; continue loop
-
-exit:
+reset_term:
             mov       rax, sys_ioctl          ; system call for ioctl
             mov       rdi, fd_stdin           ; file descriptor
             mov       rsi, tcgets             ; request to get terminal attributes
@@ -60,10 +69,7 @@ exit:
             mov       rsi, tcsets             ; request number
             mov       rdx, termios            ; termios stucture to write
             syscall                           ; invoke the OS to write termios
-
-            mov       rdi, 0                  ; store read exit code
-            mov       rax, sys_exit           ; system call for exit
-            syscall                           ; invoke operating system to exit
+            ret
 
             section   .bss
 buffer:     resb      1
